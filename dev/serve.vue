84f0b562095924ebc76d5d0b835582d573aa4a87
i.gotka <i.gotka@NTB40745.local>
2022-04-22 11:37:36 +0300

    Инициализация проекта

---
+++ dev/serve.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+import { TTUpButton, TTBackButton } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    TTUpButton,
+    TTBackButton,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <TTUpButton />
+    <TTBackButton/>
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export { default as TTUpButton } from './TTUpButton/TTUpButton.vue';
+export { default as TTBackButton } from './TTBackButton/TTBackButton.vue';
+
